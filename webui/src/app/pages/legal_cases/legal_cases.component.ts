c25bdfa1ed8345617f1ed45f0baa9728334b031e
Charif Belhaffef <charif.belhaffef@tessi.fr>
2017-12-19 18:14:38 +0100

    init project

---
+++ webui/src/app/pages/legal_cases/legal_cases.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 's-dashboard-pg',
+	templateUrl: './legal_cases.component.html',
+    styleUrls: [ './legal_cases.scss'],
+})
+
+export class LegalCasesComponent {
+  constructor( ) { }
+}
