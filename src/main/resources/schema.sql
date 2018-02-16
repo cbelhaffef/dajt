d6f583c3dba12cbe86d516c79f8ca411db794e1b
charif BELHAFFEF <charif.belhaffef@amaris.com>
2018-02-16 22:44:22 +0100

    add clearDb

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
-#DROP SCHEMA IF EXISTS dajt;
+DROP SCHEMA IF EXISTS dajt;
 
-#CREATE SCHEMA dajt;
-USE egp6cwexxvm76s5j;
+CREATE SCHEMA dajt;
+USE dajt;
 
 /* Table: user (Application Users) */
 CREATE TABLE user (
